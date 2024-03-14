f1aa29b510ecd75aebec7de02cc947cdc0fabd60
YSpoof <32226366+YSpoof@users.noreply.github.com>
2024-03-14 09:36:05 -0300

    Atualizado script de instalação
    A maior alteração foi a desativação do 'systemd-resolved' visto que causa problemas por iniciar primeiro que o Docker.

--- Instalar.sh
+++ Instalar.sh
@@ -9,8 +9,21 @@ then
   exit 1
 fi
 
+# Variables
+composeRepo="docker/compose"
+composePath="/usr/local/lib/docker/cli-plugins"
+composeFile="docker-compose-linux-x86_64"
+composeDestFile="docker-compose"
+composeLatestVersion=$(curl -s https://api.github.com/repos/$composeRepo/releases/latest | grep 'tag_name' | cut -d\" -f4)
+workDir="/opt/ADGuard"
+
 # Clear the screen
 clear
+
+# Disable systemd-resolved
+echo "Desabilitando systemd-resolved..."
+systemctl stop systemd-resolved &> /dev/null
+systemctl disable systemd-resolved &> /dev/null
 
 # Update APT repositories
 echo "Atualizando repositórios..."
@@ -23,17 +36,14 @@ apt install -y docker.io &> /dev/null
 # Install docker compose
 echo "Instalando docker compose..."
 
-# Extract docker compose latest version
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-
 # Create docker cli plugins folder
-mkdir -p /usr/local/lib/docker/cli-plugins &> /dev/null
+mkdir -p $composePath &> /dev/null
 
 # Download that version
-wget https://github.com/docker/compose/releases/download/$version/docker-compose-linux-x86_64 -O /usr/local/lib/docker/cli-plugins/docker-compose &> /dev/null
+wget https://github.com/$composeRepo/releases/download/$composeLatestVersion/$composeFile -O $composePath/$composeDestFile &> /dev/null
 
 # Mark the file as executable
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose &> /dev/null
+chmod +x $composePath/$composeDestFile &> /dev/null
 
 # Login to docker hub
 echo "Logando no docker hub..."
@@ -41,15 +51,15 @@ read -p "Digite o token da sua unidade e pressione ENTER: " dockerUnitToken
 docker login -u weducation --password-stdin <<< $dockerUnitToken &> /dev/null
 
 # Create a directory for the ADGuard compose file and CD onto it
-mkdir -p /opt/ADGuard &> /dev/null
-cd /opt/ADGuard &> /dev/null
+mkdir -p $workDir &> /dev/null
+cd $workDir &> /dev/null
 
 # Get the docker-compose.yaml file
-wget https://raw.githubusercontent.com/weducation/ADGuard/main/docker-compose.yaml -O /opt/ADGuard/docker-compose.yaml &> /dev/null
+wget https://raw.githubusercontent.com/weducation/ADGuard/main/docker-compose.yaml -O $workDir/docker-compose.yaml &> /dev/null
 
 # Down the containers just in case...
 echo "Parando containers..."
-docker compose down &> /dev/null
+docker compose down 2> /dev/null
 
 # Up the containers
 echo "Iniciando containers..."
